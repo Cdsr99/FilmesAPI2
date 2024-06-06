127d2f3e0a1af9e8978e093175e20fa88b033bf3
Caio da Silveira Rodrigues <cdsr073@gmail.com>
2024-06-06 08:50:24 -0300

    Update

--- FilmesApi/Models/Sessao.cs
+++ FilmesApi/Models/Sessao.cs
@@ -2,13 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FilmesApi.Models
 {
-    public class Endereco
+    public class Sessao
     {
         [Key]
         [Required]
         public int Id { get; set; }
-        public string Logradouro { get; set; }
-        public int Numero { get; set; }
+        [Required]
+        public int FilmeId { get; set; }
+        public virtual Filme Filme { get; set; }
+        public int? CinemaId { get; set; }
         public virtual Cinema Cinema { get; set; }
     }
 }
